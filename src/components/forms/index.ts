ae310cf5d9c25e04d5cb33a20c56dc5446f61c0e
Giuseppe Barillari <joe2k01dev@gmail.com>
2026-01-25 17:52:58 +0000

    Refactor: Remove RHF
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- src/components/forms/index.ts
+++ src/components/forms/index.ts
@@ -1,5 +1,3 @@
-export { FormInput } from "./FormInput";
-export type { FormInputProps } from "./FormInput";
 export { TextInput } from "./TextInput";
 export type { TextInputProps } from "./TextInput";
 export { Picker } from "./Picker";
