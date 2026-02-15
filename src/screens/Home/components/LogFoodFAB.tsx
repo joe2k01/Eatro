c3fe7f580ff1294e14ca6e0b4f65d214fd747231
Giuseppe Barillari <joe2k01dev@gmail.com>
2026-02-15 19:05:06 +0000

    Change FAB content
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- src/screens/Home/components/LogFoodFAB.tsx
+++ src/screens/Home/components/LogFoodFAB.tsx
@@ -30,7 +30,7 @@ export function LogFoodFAB({ visible }: LogFoodFABProps) {
   const navigation = useNavigation();
 
   const handlePress = useCallback(() => {
-    navigation.navigate("Scanner");
+    navigation.navigate("Search");
   }, [navigation]);
 
   const animatedStyle = useAnimatedStyle(() => ({
@@ -47,7 +47,9 @@ export function LogFoodFAB({ visible }: LogFoodFABProps) {
     <Animated.View style={[styles.container, animatedStyle]}>
       <Button
         variant="primary"
-        leftIcon={<Icon community name="barcode" size="xs" />}
+        leftIcon={
+          <Icon community name="receipt-text-edit-outline" size="xs" />
+        }
         onPress={handlePress}
       >
         Log food
