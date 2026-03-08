7eed14722323702f1d031478c22c84a4c11c727a
Giuseppe Barillari <joe2k01dev@gmail.com>
2026-03-08 22:37:11 +0000

    chore: lint
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- src/db/migrations/migrate.ts
+++ src/db/migrations/migrate.ts
@@ -5,7 +5,7 @@ import * as Sentry from "@sentry/react-native";
 
 // NOTE: Metro requires asset `require()` calls to be statically analyzable.
 // Do NOT build these paths dynamically (e.g. `.map(require)`), or bundling will fail.
-// eslint-disable-next-line @typescript-eslint/no-require-imports
+
 const migrations = [
   require("./initial_schema.sql"),
   require("./002_manual_foods_index.sql"),
